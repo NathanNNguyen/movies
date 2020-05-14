7ad4bb8344599e91f59f7d77f45f3ab5b5dee655
Nhan <natenguyen51294@gmail.com>
2020-05-13 21:04:57 -0700

    converted to redux

--- src/store/actions/action.js
+++ src/store/actions/action.js
@@ -1,6 +1,31 @@
+import axios from 'axios'
+
+const api = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_KEY}`;
 
 export const updateText = e => {
   return { type: 'UPDATE_TEXT', payload: e.target.value }
+}
+
+export const search = (e, query) => {
+  return dispatch => {
+    if (e.key === "Enter") {
+      axios(api + "&s=" + query).then(({ data }) => {
+        let results = data.Search;
+
+        dispatch({ type: 'SEARCH', payload: results })
+      });
+    }
+  }
+}
+
+export const openPopup = id => {
+  return dispatch => {
+    axios(api + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      dispatch({ type: 'OPEN', payload: result })
+    });
+  }
 }
 
 export const closePopup = () => {
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -5,12 +5,23 @@ const initialState = {
 }
 
 export const reducer = (state = initialState, action) => {
-  console.log(state.s)
   switch (action.type) {
     case 'UPDATE_TEXT':
       return {
         ...state,
         s: action.payload
+      }
+
+    case 'SEARCH':
+      return {
+        ...state,
+        results: action.payload
+      }
+
+    case 'OPEN':
+      return {
+        ...state,
+        selected: action.payload
       }
 
     case 'CLOSE_POPUP':
